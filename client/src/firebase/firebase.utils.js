a826e7285654699d581422f152fc078de2c2aff2
KunalMahato11 <kunalmahato11@gmail.com>
2021-08-01 11:53:59 +0530

    firebase config updated

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -2,16 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// const config = {
-//     apiKey: "AIzaSyBGCVjIv5mVMp9c-pqgRCK0a51WztrQRSU",
-//     authDomain: "crwn-db-5.firebaseapp.com",
-//     databaseURL: "https://crwn-db-5.firebaseio.com",
-//     projectId: "crwn-db-5",
-//     storageBucket: "crwn-db-5.appspot.com",
-//     messagingSenderId: "434062697851",
-//     appId: "1:434062697851:web:28bcacade8047eac6b89de",
-//     measurementId: "G-E3BPMYJK50"
-// };
 const config = {
 	apiKey: 'AIzaSyAT5GtSCo-VBRFT_xFQHCBbhl7wvCpHagg',
 	authDomain: 'storex-db.firebaseapp.com',
